07d37c7d0146d91ba496da6cc59e99f500d999be
Mads Mætzke Tandrup <mads@maetzke-tandrup.dk>
2013-02-03 15:28:58 +0100

    Included jQuery mobile and some more code to call DR

--- DRNU4WindowsPhone/app.js
+++ DRNU4WindowsPhone/app.js
@@ -1,3 +1,77 @@
+var DRModel = (function () {
+    function DRModel() {
+    }
+    DRModel.prototype.retrieveOverview = function () {
+        var _this = this;
+        $.mobile.loading('show');
+        $.getJSON("http://anyorigin.com/get?url=http%3A//www.dr.dk/NU/api/programseries&callback=?", function (seriesResult) {
+            return _this.handleOverview(seriesResult.contents);
+        });
+    };
+    DRModel.prototype.handleOverview = function (series) {
+        this.series = series;
+        var labels = {
+        };
+        $.each(this.series, function (i, serie) {
+            $.each(serie.labels, function (j, label) {
+                return labels[label] = true;
+            });
+        });
+        this.showLabels(labels);
+        $.mobile.loading('hide');
+    };
+    DRModel.prototype.showLabels = function (labels) {
+        var _this = this;
+        $("#overviewList").remove("li");
+        $.each(labels, function (key, value) {
+            var li = $('<li/>');
+            li.append($('<a/>', {
+                "data-transition": "slide",
+                text: key,
+                click: function (e) {
+                    _this.selectedLabel = key;
+                    _this.showSeries(key);
+                    $.mobile.changePage('#programs');
+                }
+            }));
+            li.appendTo("#overviewList");
+        });
+        $("#overviewList").listview('refresh');
+    };
+    DRModel.prototype.showSeries = function (label) {
+        var series = [];
+        if(label) {
+            $.each(this.series, function (i, serie) {
+                $.each(serie.labels, function (i, serieLabel) {
+                    if(label == serieLabel) {
+                        series.push(serie);
+                    }
+                });
+            });
+        } else {
+            series = this.series;
+        }
+        $("#programsList li").remove();
+        $.each(series, function (index, serie) {
+            var li = $('<li/>');
+            li.append($('<a/>', {
+                "data-transition": "slide",
+                text: serie.title,
+                click: function (e) {
+                    $.mobile.changePage('#videos');
+                }
+            }));
+            li.appendTo("#programsList");
+        });
+        //$("#programsList").listview('refresh');
+            };
+    DRModel.prototype.retrieveVideos = function () {
+        $.getJSON('http://anyorigin.com/get?url=http%3A//www.dr.dk/NU/api/programseries/max/videos&callback=?', this.handleVideos);
+    };
+    DRModel.prototype.handleVideos = function (result) {
+    };
+    return DRModel;
+})();
 var Greeter = (function () {
     function Greeter(element) {
         this.element = element;
@@ -18,6 +92,8 @@ var Greeter = (function () {
                 var videoHtml = document.createElement('video');
                 _this.element.appendChild(videoHtml);
                 videoHtml.setAttribute("src", mediaUrl);
+                videoHtml.setAttribute("poster", "http://www.dr.dk/NU/api/videos/" + video.id + "/images/400x300.jpg");
+                videoHtml.setAttribute("width", "100%");
             });
         });
     };
@@ -26,9 +102,8 @@ var Greeter = (function () {
             };
     return Greeter;
 })();
+var model = new DRModel();
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    model.retrieveOverview();
 };
 //@ sourceMappingURL=app.js.map
--- DRNU4WindowsPhone/app.ts
+++ DRNU4WindowsPhone/app.ts
@@ -1,9 +1,26 @@
 /// <reference path="jquery.d.ts" />
+/// <reference path="jquerymobile.d.ts" />
 
 interface AnyOriginResult {
     contents;
     headers;
     status;
+}
+
+interface DRProgramSerie {
+    slug: string;
+    title: string;
+    description: string;
+    shortName: string;
+    newestVideoId: number;
+    newestVideoPublishTime: string;
+    videoCount: number;
+    labels: string[];
+    webCmsImagePath: string;
+}
+
+interface DRProgramSerieResult extends AnyOriginResult {
+    contents: DRProgramSerie[];
 }
 
 interface DRVideo {
@@ -38,6 +55,85 @@ interface DRVideoResult extends AnyOriginResult {
     contents: DRVideo[];
 }
 
+class DRModel {
+    series: DRProgramSerie[];
+    selectedLabel: string;
+
+    constructor() {
+    }
+
+    retrieveOverview() {
+        $.mobile.loading('show');
+        $.getJSON("http://anyorigin.com/get?url=http%3A//www.dr.dk/NU/api/programseries&callback=?", (seriesResult: DRProgramSerieResult) => this.handleOverview(seriesResult.contents));
+    }
+
+    private handleOverview(series: DRProgramSerie[]) {
+        this.series = series;
+        var labels = {};
+        $.each(this.series, (i, serie: DRProgramSerie) => {
+            $.each(serie.labels, (j, label: string) => labels[label] = true);
+        });
+
+        this.showLabels(labels);
+        $.mobile.loading('hide');
+    }
+
+    private showLabels(labels) {
+        $("#overviewList").remove("li");
+        $.each(labels, (key: string, value) => {
+            var li = $('<li/>');
+            li.append($('<a/>', {
+                "data-transition": "slide",
+                text: key,
+                click: (e) => {
+                    this.selectedLabel = key;
+                    this.showSeries(key);
+                    $.mobile.changePage('#programs');
+                }
+            }));
+            li.appendTo("#overviewList");
+        });
+
+        $("#overviewList").listview('refresh');
+    }
+
+    private showSeries(label: string) {
+        var series = [];
+        if (label) {
+            $.each(this.series, (i, serie: DRProgramSerie) => {
+                $.each(serie.labels, (i, serieLabel: string) => {
+                    if (label == serieLabel) {
+                        series.push(serie);
+                    }
+                });
+            });
+        } else {
+            series = this.series;
+        }
+
+        $("#programsList li").remove();
+        $.each(series, (index, serie: DRProgramSerie) => {
+            var li = $('<li/>');
+            li.append($('<a/>', {
+                "data-transition": "slide",
+                text: serie.title,
+                click: (e) => {
+                    $.mobile.changePage('#videos');
+                }
+            }));
+            li.appendTo("#programsList");
+        });
+    }
+
+    retrieveVideos() {
+        $.getJSON('http://anyorigin.com/get?url=http%3A//www.dr.dk/NU/api/programseries/max/videos&callback=?', this.handleVideos);
+    }
+
+    private handleVideos(result: DRVideoResult) {
+
+    }
+}
+
 class Greeter {
     element: HTMLElement;
     span: HTMLElement;
@@ -63,6 +159,8 @@ class Greeter {
         var videoHtml = document.createElement('video');
         this.element.appendChild(videoHtml);
         videoHtml.setAttribute("src", mediaUrl);
+        videoHtml.setAttribute("poster", "http://www.dr.dk/NU/api/videos/" + video.id + "/images/400x300.jpg");
+        videoHtml.setAttribute("width", "100%");
     });
 });
     }
@@ -73,8 +171,8 @@ class Greeter {
 
 }
 
+var model = new DRModel();
+
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    model.retrieveOverview();
 };
